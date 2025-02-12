6c7e16ca8ed6ce163c4cbe33063289870d27d3d0
Tori Watkis <tori.watkis@datadoghq.com>
2025-02-12 18:20:30 -0500

    added borders on word pills as a design update

--- frontend/src/app/components/Doing/VerbList.js
+++ frontend/src/app/components/Doing/VerbList.js
@@ -3,22 +3,22 @@ const VerbList = () => {
     return (
         <div className='bg-yellow-400 flex justify-stretch  place-items-stretch m-4 ml-0 flex relative w-full h-dvh'>
             <div class="grid grid-cols-4 gap-4 relative w-full p-6">
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Eat</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Walk</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Swim</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Write</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Code</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Drive</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Race</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Drink</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Watch</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Blink</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Think</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Run</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Stare</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Play</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Clean</div>
-                <div className='bg-yellow-600 rounded-full text-center content-center text-white uppercase font-bold'>Sleep</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Eat</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Walk</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Swim</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Write</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Code</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Drive</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Race</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Drink</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Watch</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Blink</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Think</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Run</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Stare</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Play</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Clean</div>
+                <div className='bg-yellow-600 border-yellow-700 border-4 rounded-full text-center content-center text-white uppercase font-bold'>Sleep</div>
             </div>
         </div>
     )
