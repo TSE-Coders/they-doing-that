35cbbe0e8a353372310e99c22e0e547ba63083d4
Tori Watkis <tori.watkis@datadoghq.com>
2025-02-12 17:50:37 -0500

    got they word display working

--- frontend/src/app/components/They/TheyForm.js
+++ frontend/src/app/components/They/TheyForm.js
@@ -55,13 +55,13 @@ async function TheyDelete(subjects){
 const TheyForm = ({subjects}) => {
     return (
         <div className='flex justify-stretch  flex-col w-full h-dvh border-b-4 border-red-950 m-0'>
-        <form onSubmit={TheyPost} className="flex flex-col justify-stretch w-full mt-36 mb-0">
+        <form onSubmit={TheyPost} className="flex flex-col justify-stretch w-full mt-36 mb-0 p-0">
           <label id="subject" className="flex items-center gap-8">
-            <input type="text" placeholder="add word here" className="input input-bordered w-full ml-20 mr-20" />
+            <input type="text" placeholder="add word here" className="input input-bordered w-full ml-20 mr-20 mt-0 mb-0" />
           </label>
-          <button type="submit" className="btn ml-36 mr-36 mt-8 mb-0 border-0 bg-red-800 rounded-full">add a subject</button>
+          <button type="submit" className="btn ml-36 mr-36 mt-8 mb-0 pb-0 border-0 bg-red-800 rounded-full">add a subject</button>
         </form>
-          <button onClick={() => TheyDelete(subjects)} className="btn  ml-36 mr-36 mt-0 border-0 bg-red-800 rounded-full">remove a subject</button>
+          <button onClick={() => TheyDelete(subjects)} className="btn  ml-36 mr-36 mt-0 pt-0 border-0 bg-red-800 rounded-full">remove a subject</button>
         </div>
     )
 }
--- frontend/src/app/components/They/TheyWordDisplay.js
+++ frontend/src/app/components/They/TheyWordDisplay.js
@@ -1,8 +1,45 @@
 'use client'
+import {  useState, useEffect } from 'react'
+
 const TheyWordDisplay = () => {
+    const [data, setData] = useState([])
+    const [isLoading, setLoading] = useState(true)
+    const [check, setCheck] = useState(0)
+
+  //FETCH RANDOM NAME
+  async function fetchName() {
+    try {
+      const res = await fetch(`/api/getRandomName`);
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status} API unreachable`);
+      }
+  
+      const payload = await res.json();
+      if (payload && payload.data) {
+        setData(payload.data);
+        setLoading(false);
+      } else {
+        setData({ name: "no data available" });
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error("Error fetching name:", error);
+      setData({ name: "no data available" });
+      setLoading(false);
+    }
+  }
+ useEffect(() => {  
+    
+  const id = setInterval(() => {
+                fetchName()
+                setCheck(check + 1)
+              }, 3000);
+  return () => clearInterval(id);            
+  },[check]) 
+
     return (
         <div className="bg-red-700 flex flex-col justify-center items-center  place-items-stretch m-0 flex relative w-full h-dvh border-r-4 border-red-950">
-            <p className='text-8xl text-wrap font-black text-red-950 uppercase text-center p-36'>Tori</p>
+            <p className='text-8xl text-wrap font-black text-red-950 uppercase text-center p-36'>{data.name}</p>
             <div className='relative m-10 mt-44 justify-center content-end'>
                 <p className='text-center text-base text-sm text-red-950'>Powered by:</p>
                 <p className='text-center text-base font-bold text-red-950 uppercase'>Ruby x Postgres</p>
