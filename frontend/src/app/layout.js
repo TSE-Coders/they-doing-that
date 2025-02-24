a30c6c6e684d6cc4a9cb61ba9ad562d68ba5103b
Tori Watkis <tori.watkis@datadoghq.com>
2025-02-24 13:09:08 -0500

    implementation of Datadog RUM in next.js application

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import DatadogInit from "./components/datadog-init";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,8 +22,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
+    
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <DatadogInit />
         {children}
       </body>
     </html>
