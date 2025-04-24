af43232bf147941352d26c03235a3873305e47ef
Tori Watkis <tori.watkis@datadoghq.com>
2025-04-24 14:56:19 -0400

    commit

--- subject-service/rubyonrails-api/config/initializers/datadog.rb
+++ subject-service/rubyonrails-api/config/initializers/datadog.rb
@@ -1,18 +1,6 @@
 # require 'datadog'
 
 
-<<<<<<< HEAD
-Datadog.configure do |c|
-  # Add additional configuration here.
-  # Activate integrations, change tracer settings, etc...
-  c.tracing.instrument :rails
-  c.service = 'they-api'
-  c.env = 'tdt'
-  c.tracing.log_injection = true
-  c.agent.port = 8136
-  c.tracing.instrument :pg, comment_propagation: 'service'
-  c.diagnostics.startup_logs.enabled = false
-=======
 # Datadog.configure do |c|
 #   # Add additional configuration here.
 #   # Activate integrations, change tracer settings, etc...
@@ -23,6 +11,5 @@ Datadog.configure do |c|
 #   c.agent.port = 8136
 #   c.tracing.instrument :pg, comment_propagation: 'service'
 #   c.diagnostics.startup_logs.enabled = false
->>>>>>> f4141aad2aff907e61b093827683f6b3961166ec
   
 # end 
